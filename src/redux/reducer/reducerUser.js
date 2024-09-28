ef033d84b7538d14454dc531dc088031f55123bc
your name <abukarakiomar@gmail.com>
2024-09-28 20:45:22 +0300

    push last updates

--- src/redux/reducer/reducerUser.js
+++ src/redux/reducer/reducerUser.js
@@ -2,8 +2,11 @@ import { ALLURERS } from "../type/userType"
 
 const iniState =  {data:[]};
 export const reducerUser = (state=iniState, action)=>{
+    
     if(action.type === ALLURERS){
         return {type:ALLURERS,data:action.data}
+    }if(action.type === 'oneUser'){
+        return {type:'oneUser',data:[{name:'omar'}]}
     }
     return state;
 }
